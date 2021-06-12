e3b6686e36383ff487069a729d8515574cc18ccc
RPapin <remi.papin@viacesi.fr>
2021-06-12 12:18:37 +0200

    add temp file

--- urlApi.py
+++ urlApi.py
@@ -2,21 +2,25 @@ from flask import Flask, request
 import json
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+import os
 
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 cache = {}
+urlFile = "tunnelUrl.txt"
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>ACC randomize app URL Api</h1>"
 # A route to return all data.
 @app.route('/get_url', methods=['GET'])
 def start_championnship():
-    if "tunnel_url" in cache:
+    if os.path.isfile(urlFile) : 
+        f = open(urlFile, "r")
+        url = f.read()
         return jsonify({
-            "url" : cache['tunnel_url']
+            "url" : url
             })
     else :
         return jsonify({
@@ -26,8 +30,11 @@ def start_championnship():
 def display_result():
     data = request.form
     if "tunnel_url" in data:
-        cache.pop('tunnel_url', None)
-        cache['tunnel_url'] = data['tunnel_url']
+        # cache.pop('tunnel_url', None)
+        # cache['tunnel_url'] = data['tunnel_url']
+        f = open(urlFile, "w")
+        f.write(data['tunnel_url'])
+        f.close()
         return "success"
     else :
         return "Error. Post data not correct"
